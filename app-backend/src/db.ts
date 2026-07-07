c98a41f09b6e8be8e352e60aaa758c9ebbcb30c8
Diego <diego.ordonez.becerril@gmail.com>
2026-07-06 23:26:14 -0600

    Remove console log

--- app-backend/src/db.ts
+++ app-backend/src/db.ts
@@ -3,7 +3,6 @@ import { MongoClient, ObjectId, Collection } from 'mongodb';
 const { MONGO_USER, MONGO_PASSWORD, MONGO_HOST, MONGO_DB } = process.env;
 const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:27017/${MONGO_DB}?authSource=${MONGO_DB}`;
 
-console.log('Connecting to MongoDB:', mongoUrl);
 const dbName = MONGO_DB;
 const collectionName = 'subjects';
 
